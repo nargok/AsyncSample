e4b6de17c5c99e7745f5b66ba2b6ac9e904dd125
Ryoichi Gokan <nargok@gmail.com>
2018-03-10 12:04:17 +0900

    非同期処理を追記

--- app/src/main/java/sample/asyncsample/WeatherInfoActivity.java
+++ app/src/main/java/sample/asyncsample/WeatherInfoActivity.java
@@ -1,5 +1,6 @@
 package sample.asyncsample;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -68,7 +69,60 @@ public class WeatherInfoActivity extends AppCompatActivity {
             // 取得した都市名をtvCityNameに設定
             TextView tvCityName = findViewById(R.id.tvCityName);
             tvCityName.setText(cityName + "の天気: ");
+            // 天気情報を表示するTextViewを取得
+            TextView tvWeatherTelop = findViewById(R.id.tvWeatherTelop);
+            // 天気詳細情報を表示するTextViewを取得
+            TextView tvWeatherDesc = findViewById(R.id.tvWeatherDesc);
+            // WeatherInfoReceiverをnew。引数として上で取得したTextViewを渡す。
+            WeatherInfoReceiver receiver = new WeatherInfoReceiver(tvWeatherTelop, tvWeatherDesc);
+            // WeatherInfoReceiverを実行
+            receiver.execute(cityId);
+        }
+    }
 
+    // AsyncTaskの引数
+    // 1:execute(), doInBackgroundの引数の型
+    // 2: publishProgress(), onProgressUpdate(O)の引数の型
+    // 3: doInBackground, onPostExecute, onCancelledの引数の型
+    private class WeatherInfoReceiver extends AsyncTask<String, String, String> {
+        // 現在の天気を表示する画面部品フィールド
+        private TextView _tvWeatherTelop;
+        // 天気の詳細を表示する画面部品フィールド
+        private TextView _tvWeatherDesc;
+
+        // コンストラクタ 記述しておくことでnewの時の引数で画面描画部分を取得できるから便利
+        public WeatherInfoReceiver(TextView tvWeatherTelop, TextView tvWeatherDesc) {
+            _tvWeatherTelop = tvWeatherTelop;
+            _tvWeatherDesc = tvWeatherDesc;
+        }
+
+        @Override
+        public String doInBackground(String... params) {
+            // 可変長引数の1個目(インデックス0)を取得。これが都市ID
+            String id = params[0];
+            //　都市IDを使って接続URLを作成
+            String urlStr = "http://weather.livedoor.com/forecast/webservise/json/v1?city=" + id;
+            // 天気情報サービスから取得したJSON文字列。天気情報が格納されている。
+            String result = "";
+
+            // ここに上記URLに接続してJSON文字列を取得する処理を記述
+
+            // JSON文字列を返す
+            return result;
+        }
+
+        @Override
+        public void onPostExecute(String result) {
+            // 天気情報用文字列変数を用意
+            String telop = "";
+            String desc = "";
+
+            // ここに天気情報文字列を解析する処理を記述
+
+            // 的情報用文字列をTextViewにセット
+            _tvWeatherTelop.setText(telop);
+            _tvWeatherDesc.setText(desc);
         }
     }
 }
+
